480a4c2c6a57e04041e2912c92a3dcc9cb4c71dc
Andrei <andrei_1002@hotmail.com>
2021-07-19 21:19:15 +0200

    created routes and views for log creation

--- controllers/log.controllers.js
+++ controllers/log.controllers.js
@@ -1,11 +1,35 @@
-const User = require('../models/user.model');
-const Log = require('../models/log.model');
+const User = require("../models/user.model");
+const Log = require("../models/log.model");
 
 module.exports.viewLog = (req, res, next) => {
   const { id } = req.params;
- Log.findById(id)
- .then((log) => {
+  Log.findById(id).then((log) => {
+    res.render("detail", { log: log });
+  });
+};
 
-  res.render('detail', {log: log})
- })
-}
+module.exports.create = (req, res, next) => {
+  res.render("new-log");
+};
+
+module.exports.doCreate = (req, res, next) => {
+  req.body.owner = req.user._id;
+
+  if (req.file) {
+    req.body.image = req.file.path;
+  }
+
+  if (req.body.tags) {
+    req.body.tags = req.body.tags.split(",").map((tag) => tag.trim());
+  }
+
+  Log.create(req.body)
+    .then(() => {
+      res.redirect(`/users/${req.user._id}`);
+    })
+    .catch(() => {
+      res.render("new-log");
+    });
+};
+
+
--- routes/logs.routes.js
+++ routes/logs.routes.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
-const upload = require('../config/storage.config');
-const logControllers = require('../controllers/log.controllers')
+const router = require("express").Router();
+const upload = require("../config/storage.config");
+const logControllers = require("../controllers/log.controllers");
 
-router.get('/:id', logControllers.viewLog)
+router.get("/newlog", logControllers.create);
+router.post("/newlog", upload.single("image"), logControllers.doCreate);
 
-
+router.get("/:id", logControllers.viewLog);
 
 module.exports = router;
