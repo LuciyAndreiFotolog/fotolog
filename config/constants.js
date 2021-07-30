8e6a470e3cc45c600dc128a053a361607ee3ba38
Andrei <andrei_1002@hotmail.com>
2021-07-30 11:31:47 +0200

    bug fixed

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 const DB_NAME = 'fotolog';
-const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const URI = 'mongodb://localhost:27017';
 const DB_URI = `${URI}/${DB_NAME}`;
 
 module.exports.dbName = DB_NAME;
 module.exports.db = DB_URI;
+
+// process.env.MONGODB_URI
